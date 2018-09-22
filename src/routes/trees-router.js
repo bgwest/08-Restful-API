4019a8cd7022f52e36a63dc7b48669f4f45e28ce
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2018-09-21 23:26:31 -0700

    added a POST path to retrieve username by id

--- src/routes/trees-router.js
+++ src/routes/trees-router.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//! Vinicio - I'm using capital U because User is a class
 const path = require('path');
 const fs = require('fs');
 const User = require('../model/user-template');
@@ -9,6 +8,16 @@ const logger = require('../lib/logger');
 
 const userStorage = {
   table: [],
+};
+
+const writeUserFile = (data, callback) => {
+  const dataFile = path.join(__dirname, '../data/user.session.json');
+  fs.writeFile(`${dataFile}`, data, (error) => {
+    if (error) {
+      throw error;
+    }
+    return callback;
+  });
 };
 
 const sendStatus = (statusCode, message, response) => {
@@ -27,17 +36,7 @@ const sendJSON = (statusCode, data, response) => {
 };
 
 //! Vinicio - this route is going to be used to CREATE notes
-app.post('/api/notes', (request, response) => { //! Vinicio - this is what gets passed as callback
-  //! Vinicio - at this point we know
-  // - we have a post request : the user wants to create a new user
-  // - the request has bee matched to this function
-
-  // ALGO:
-  // validate all the input (i.e. request)
-  // create a user
-  //----------------------------------------------------------------------------------
-  // REQUEST VALIDATION
-  //----------------------------------------------------------------------------------
+app.post('/api/notes', (request, response) => {
   if (!request.body) {
     sendStatus(400, 'body not found', response);
     return undefined;
@@ -56,12 +55,12 @@ app.post('/api/notes', (request, response) => { //! Vinicio - this is what gets 
   // User CREATION
   //----------------------------------------------------------------------------------
   const user = new User(request.body.username, request.body.title);
-  userStorage.table.push(user); //! Vinicio - eventually, we'll add the note into our DB
+  userStorage.table.push(user);
   sendJSON(200, user, response);
   return undefined;
 });
 
-app.post('/get/id', (request, response) => {
+app.post('/new/user', (request, response) => {
   // ALGO:
   // validate all the input (i.e. request)
   // create a user
@@ -72,7 +71,7 @@ app.post('/get/id', (request, response) => {
     sendStatus(400, 'body not found', response);
     return undefined;
   }
-  //! Vinicio - making sure I have all the information I need to create a new user
+  // ensure all the information I need to create a new user
   if (!request.body.username) {
     sendStatus(400, 'title not found', response);
     return undefined;
@@ -87,29 +86,30 @@ app.post('/get/id', (request, response) => {
   //----------------------------------------------------------------------------------
   const user = new User(request.body.username, request.body.title);
   logger.log(logger.INFO, `new user = ${Object.keys(user)}`);
-  userStorage.table.push(user); //! Vinicio - eventually, we'll add the note into our DB
+  userStorage.table.push(user);
   sendJSON(200, user, response);
+  // write data to json file
+  const stringIfyUserStorage = JSON.stringify(userStorage);
+  logger.log(logger.INFO, `userStorage = ${stringIfyUserStorage}`);
+  writeUserFile(stringIfyUserStorage);
   return undefined;
 });
 
-const writeUserFile = (data, callback) => {
-  const dataFile = path.join(__dirname, '../data/user.session.json');
-  fs.writeFile(`${dataFile}`, data, (error) => {
-    if (error) {
-      throw error;
+app.post('/login', (request, response) => {
+  logger.log(logger.INFO, `new user = ${request.body.id}`);
+  for (let searchTable = 0; searchTable <= userStorage.table.length - 1; searchTable++) {
+    if (userStorage.table[searchTable].id === request.body.id) {
+      // we can trust this id because it is unique
+      response.write(`Hello, ${userStorage.table[searchTable].username}.`);
+      response.end();
     }
-    return callback;
-  });
-};
+  }
+  return undefined;
+});
+
 
 app.get('/', (request, response) => {
-  const stringIfyUserStorage = JSON.stringify(userStorage);
-  logger.log(logger.INFO, `userStorage = ${stringIfyUserStorage}`);
-  // fs.writeFile('data.json', '{"testing":"123"}', (err) => {
-  //   if (err) throw err;
-  //   console.log('Updated user file.');
-  // });
-  writeUserFile(stringIfyUserStorage);
-
-  sendJSON(200, '../data/data.json', response);
+  response.write('<!DOCTYPE><header></header><body><div><p>cool beans.</p></div></body></html>');
+  response.end();
+  return undefined;
 });
